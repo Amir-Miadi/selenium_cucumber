4d8ecdd2aff96accccac0bbf6415846c3f00648f
Amir-Miadi <miadi.amir@gmail.com>
2023-02-18 00:22:10 +0100

    Update Project

--- src/specs/features/featurefile/loginpage.feature
+++ src/specs/features/featurefile/loginpage.feature
@@ -3,7 +3,7 @@ Feature: Authentification de login de l application nopcommerce
   En tant que utilisateur  je souhaite naviger
 
   @login_CasPassant
-  Scenario: Authentification sur l application SwagLabs- Cas Passant
+  Scenario: Authentification sur l application nopcommerce- Cas Passant
     Given je me connecte sur l application nopcommerce
     When je saisis le username "admin@yourstore.com"
     And je saisis le mot de pass "admin"
--- src/test/java/com/e2eTest/automation/step_definitions/loginStepDefinition.java
+++ src/test/java/com/e2eTest/automation/step_definitions/loginStepDefinition.java
@@ -1,33 +1,44 @@
 package com.e2eTest.automation.step_definitions;
 
+import com.e2eTest.automation.page_objects.LoginPage;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class loginStepDefinition {
+public class LoginStepDefinition {
+	
+	public LoginPage loginPage ;
+	public LoginStepDefinition() {
+		loginPage = new LoginPage ()	;
+	}
 
 	@Given("je me connecte sur l application nopcommerce")
 	public void jeMeConnecteSurLApplicationNopcommerce() {
+		loginPage.goToUrl();
 
 	}
 
 	@When("je saisis le username {string}")
-	public void jeSaisisLeUsername(String string) {
-
+	public void jeSaisisLeUsername(String email) {
+		loginPage.fillEmail(email);
 	}
 
 	@When("je saisis le mot de pass {string}")
-	public void jeSaisisLeMotDePass(String string) {
+	public void jeSaisisLeMotDePass(String password) {
+		loginPage.fillPassword(password);
 
 	}
 
 	@When("je clique sur le bouton login")
-	public void jeCliqueSurLeBoutonLogin() {
+	public void jeCliqueSurLeBoutonLogin(){
+		loginPage.clickOnBtnLogin();
 
 	}
 
 	@Then("je me redirige vers la page home {string}")
 	public void jeMeRedirigeVersLaPageHome(String string) {
+		
 
 	}
 
